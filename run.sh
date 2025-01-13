16998a345e9fe64720f4eac93bc7087be1624a25
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-01-13 11:05:58 +0100

    Create k8s cluster

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Install pre-requisite for fink ci
+
+# @author  Fabrice Jammes
+
+set -euxo pipefail
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+ciux_version=v0.0.4-rc10
+export CIUXCONFIG=$HOME/.ciux/ciux.sh
+go install github.com/k8s-school/ciux@"$ciux_version"
+
+echo "Ignite the project using ciux"
+mkdir -p ~/.ciux
+ciux ignite --selector itest
+
+cluster_name=$(ciux get clustername $DIR/..)
+monitoring=false
+
+# Get kind version from option -k
+while getopts mk: flag
+do
+    case "${flag}" in
+        k) kind_version_opt=--kind-version=${OPTARG};;
+        m) monitoring=true;;
+    esac
+done
+
+ktbx install kind
+ktbx install kubectl
+ktbx install helm
+ink "Create kind cluster"
+ktbx create -s --name $cluster_name
+ink "Install ArgoCD operator"
+ktbx install argocd
+
